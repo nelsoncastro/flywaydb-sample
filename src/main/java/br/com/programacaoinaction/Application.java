914db6bc4165102ee031b5a7e24f007df518c400
Nelson Pereira de Castro <nelsoncastro.developer@gmail.com>
2018-07-13 23:25:16 -0300

    Primeiro commit

---
+++ src/main/java/br/com/programacaoinaction/Application.java
@@ -0,0 +1,14 @@
+package br.com.programacaoinaction;
+
+/**
+ *
+ * @author <a href='http://programmerinaction'>Nelson Castro</a>
+ * @since 1.0
+ */
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+
+}
